3a41ab881fd4c5bd1cff7b3d78e337c632eb6501
MsJennyGiraffe <jenniferlsoden@gmail.com>
2017-03-27 15:13:41 -0600

    Got cucumber working

--- features/step_definitions/tasks_index.rb
+++ features/step_definitions/tasks_index.rb
@@ -1,38 +1,11 @@
 # require 'selenium-webdriver'
-#
+
 # driver = Selenium::WebDriver.for :firefox
 
-# Given(/^We navigate to the task index view$/) do
-#   # driver.navigate.to("localhost:3000/tasks")
-#   visit(root_path)
-#   puts "Anything -- I don't..."
-# end
-
-# When(/^We are there$/) do
-#
-# end
-#
-# Then(/^We see a list of task names and descriptions$/) do
-#   wait = Selenium::Webdriver::Wait.new(:timeout => 5)
-#   begin
-#     element = wait.until { driver.find_element({class: "tasks"}) }
-#     expect(element.text).to eq("Tasks")
-#   ensure
-#     driver.quit
-#   end
-# end
-
-When(/^I go to the homepage$/) do
- visit root_path
+Given(/^We navigate to the task index view$/) do
+  visit tasks_path
 end
 
-Then(/^I should see the welcome message$/) do
- expect(page).to have_content("Hello Cucumber")
+Then(/^We see a list of task names and descriptions$/) do
+  expect(page).to have_content("Tasks")
 end
-
-#
-# Feature: Task index
-# Scenario:
-#   Given We navigate to the task index view
-#   When We are there
-#   Then We see a list of task names and descriptions
--- features/tasks_index.feature
+++ features/tasks_index.feature
@@ -1,8 +1,4 @@
-Feature: Hello Cucumber
-As a product manager
-I want our users to be greeted when they visit our site
-So that they have a better experience
-
-Scenario: User sees the welcome message
-When I go to the homepage
-Then I should see the welcome message
+Feature: Task index
+Scenario:
+  Given We navigate to the task index view
+  Then We see a list of task names and descriptions
